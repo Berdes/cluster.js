72abedbc45fc2d216a29c1c230a21b465740e46e
Bernard Desmyter <bernarddesmyter@gmail.com>
2015-05-17 00:45:02 +0200

    Début du worker master

---
+++ master.js
@@ -0,0 +1,34 @@
+var net = require('net');
+var os = require('os');
+
+function jsWrite(s, obj, cb) {
+  if(cb !== undefined) {
+    s.write(JSON.stringify(obj, cb));
+  } else {
+    s.write(JSON.stringify(obj));
+  }
+}
+
+var workers = {};
+var addr = {};
+
+var server = net.createServer(function(socket) {
+  var ip = socket.remoteAddress;
+  if(workers[ip] !== undefined) {
+    // worker already spawned, kill it
+    jsWrite(socket, {action: 'kill'});
+  } else {
+    workers[ip] = {socket: socket};
+    socket.on('data', function(data) {
+      console.log('Recieved datas from %s : %s', ip, data);
+    });
+    socket.on('close', function() {
+      workers[ip] = undefined;
+    });
+  }
+});
+
+server.listen(function() {
+  addr.port = server.adress().port;
+  addr.ip = os.hostname();
+});
