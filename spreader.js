0d9fa9f861d06ff0ab0b8f8fd4323f184dd7104c
Bernard Desmyter <bernarddesmyter@gmail.com>
2015-05-17 14:36:45 +0200

    Correction du spreader

--- spreader.js
+++ spreader.js
@@ -6,7 +6,12 @@ var targetHost = process.argv[2];
 var masterHost = process.argv[3];
 var masterPort = process.argv[4];
 
-fs.readFile('config.json', function(rawConfig) {
+fs.readFile('config.json', function(err, rawConfig) {
+  if(err !== null) {
+    process.stderr.write(err);
+    process.stdout.write('Impossible de lire le fichier de configuration\n');
+    process.exit(1);
+  }
   var config = JSON.parse(rawConfig);
   exec(['ssh', targetHost, config.startWorker.cmd, masterHost, masterPort].join(' '),
        config.startWorker.options,
