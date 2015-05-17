ad57228117cd7698f653f0eef0e4035057d3f732
Bernard Desmyter <bernarddesmyter@gmail.com>
2015-05-17 16:18:30 +0200

    Bonne gestion du spread

--- spreader.js
+++ spreader.js
@@ -8,16 +8,16 @@ var masterPort = process.argv[4];
 
 fs.readFile('config.json', function(err, rawConfig) {
   if(err !== null) {
-    process.stderr.write(err);
+    process.stderr.write(err.toString());
     process.stdout.write('Impossible de lire le fichier de configuration\n');
     process.exit(1);
   }
   var config = JSON.parse(rawConfig);
-  exec(['ssh', targetHost, config.startWorker.cmd, masterHost, masterPort].join(' '),
+  exec(['ssh', targetHost, config.startWorker.cmd, config.pwd, masterHost, masterPort].join(' '),
        config.startWorker.options,
        function(err, stdout, stderr) {
     if(err !== null) {
-      process.stderr.write(err);
+      process.stderr.write(err.toString());
       process.stderr.write(stderr);
       process.stdout.write(stdout);
       process.exit(1);
--- startWorker.sh
+++ startWorker.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-node worker.js $1 $2 &
+cd $1
+node worker.js $2 $3 </dev/null >/dev/null 2>/dev/null &
