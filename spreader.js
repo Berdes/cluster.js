6ab4f289a51f5ba69db9879e428cb118a7906b33
Bernard Desmyter <bernarddesmyter@gmail.com>
2015-05-17 14:32:17 +0200

    Erreur de majuscule

--- spreader.js
+++ spreader.js
@@ -8,7 +8,7 @@ var masterPort = process.argv[4];
 
 fs.readFile('config.json', function(rawConfig) {
   var config = JSON.parse(rawConfig);
-  exec(['ssh', targetHost, config.startWorker.Cmd, masterHost, masterPort].join(' '),
+  exec(['ssh', targetHost, config.startWorker.cmd, masterHost, masterPort].join(' '),
        config.startWorker.options,
        function(err, stdout, stderr) {
     if(err !== null) {
